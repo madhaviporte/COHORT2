b494ef6733c7894429cf6ad38c0e08845b6e034a
madhaviporte <madhaviporte2004@gamil.com>
2025-10-29 21:37:52 +0530

    change

---
+++ Day-29/script.js
@@ -0,0 +1,8 @@
+// var a = "harsh";
+// let b = "sheriyansh";
+// const c = "javascript";
+
+console.log("bilaspur");
+console.info("bilaspur");
+console.warn("bilaspur");
+console.error("bilaspur");
