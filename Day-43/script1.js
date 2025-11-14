de38577a2b87f6f982660a59a67d89851709dbd7
madhaviporte <madhaviporte2004@gamil.com>
2025-11-14 22:07:24 +0530

    summit

---
+++ Day-43/script1.js
@@ -0,0 +1,3 @@
+var arr = ['vedant','abhi','madhavi','sumit','bhumika','anjali','kasturi','ritik','sudha','udisha','kanika','rakhi']
+var a = Math.floor(Math.random()*arr.length);
+console.log(arr[a]);
