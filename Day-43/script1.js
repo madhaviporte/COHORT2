9a2ea7730f483a4b61300c57017e3ede1a56ea1b
madhaviporte <madhaviporte2004@gamil.com>
2025-11-15 16:22:28 +0530

    change

--- Day-43/script1.js
+++ Day-43/script1.js
@@ -1,3 +1,50 @@
-var arr = ['vedant','abhi','madhavi','sumit','bhumika','anjali','kasturi','ritik','sudha','udisha','kanika','rakhi']
-var a = Math.floor(Math.random()*arr.length);
-console.log(arr[a]);
+
+
+
+// var btn = document.querySelector('button')
+// var arr = ['vedant','abhi','madhavi','sumit','bhumika','anjali','kasturi','ritik','sudha','udisha','kanika','rakhi']
+
+
+// btn.addEventListener('click', function(){
+//       var a = Math.floor(Math.random()* arr.length)
+
+//       console.log(arr[a]);
+      
+// })
+
+var arr = [
+    {
+        team:'CSK',
+        primary:'yellow',
+        secondary:'blue'
+    },
+    {
+        team:'RCB',
+        primary:'red',
+        secondary:'black'
+    },
+    {
+        team:'MI',
+        primary:'blue',
+        secondary:'gold'
+    },
+    {
+        team:'KKR',
+        primary:'purple',
+        secondary:'gold'
+    }
+]
+// console.log(arr[num].team);
+
+var btn = document.querySelector('button')
+var h1 = document.querySelector('h1')
+var main = document.querySelector('main')
+
+btn.addEventListener('click', function(){
+
+    var winner= arr[Math.floor(Math.random()*arr.length)]
+    h1.innerHTML= winner.team
+    h1.style.backgroundColor=winner.secondary
+    main.style.backgroundColor=winner.primary
+    
+})
