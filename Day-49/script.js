c7666d29282c937021963a4f03b567b24492b489
madhaviporte <madhaviporte2004@gamil.com>
2025-11-25 22:12:22 +0530

    Day-49

--- Day-49/script.js
+++ Day-49/script.js
@@ -1,1 +1,20 @@
-var AllH1 =document.querySelectorAll('h1')
+// var allH1 = document.querySelectorAll('h1')
+// console.log(h1);
+
+// var arr = [10,20,50,60]
+// console.log(arr);
+
+// allH1.push('hello')
+// console.log(allH1);
+
+// var h1 = document.querySelectorAll('h1')
+
+// console.log(h1[0]);
+// h1[2].innerHTML = 'Bye bye'
+
+var allElem = document.querySelectorAll('.elem')
+
+allElem.forEach(function(){
+    console.log(elem.childNodes);
+    
+})
