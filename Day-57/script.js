de1119a35fcee0efdcdc15d96fa3fa0e34bd455c
madhaviporte <madhaviporte2004@gamil.com>
2025-12-05 19:09:46 +0530

    Day-57

---
+++ Day-57/script.js
@@ -0,0 +1,12 @@
+// class Bottle{
+//     constructor(){
+//         this.color ="blue";
+//         this.material = "plastic";
+//         this.material ="plastic";
+//         this.price = 132;
+//     }
+//     fill(){
+//     }
+//     drink(){}
+// }
+// let bottle = new Bottle();
