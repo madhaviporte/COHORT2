b6e32399306f301e44c80b3a068f9a5c265a737c
madhaviporte <madhaviporte2004@gmail.com>
2026-01-11 23:20:36 +0530

    Day-76

---
+++ Day-76/class8-small-topics/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello guys</h1>
+      <h2>kaise ho app log</h2>
+    </div>
+  )
+}
+
+export default App
