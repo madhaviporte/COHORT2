7730e08c62c35321320e66b9a6f617ae8adcbc3e
madhaviporte <madhaviporte2004@gmail.com>
2026-01-29 19:32:33 +0530

    Change

--- Day-90/server.js
+++ Day-90/server.js
@@ -1,9 +1,16 @@
 // server ko start krna
+// database se connect krna
 const app = require('./src/app')
+
 const mongoose = require("mongoose")
 function connectToDb(){
     mongoose.connect("")
+    .then(()=>{
+        console.log("Connected to Database");
+        
+    })
 }
+connectToDb()
 
 app.listen(3000, ()=>{
     console.log("server is running on port 3000");
