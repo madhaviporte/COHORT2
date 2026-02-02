886bd74bc9a10e72f1240394d7f2d7f3eb495352
madhaviporte <madhaviporte2004@gmail.com>
2026-02-02 21:31:52 +0530

    Day-93

---
+++ Day-93/Backend/src/app.js
@@ -0,0 +1,65 @@
+//server create krna
+
+const express = require("express")
+const noteModel = require('./models/note.model')
+
+const app = express()
+app.use(express.json()) //middlewere
+
+// PORT /api/note
+//create new note and save data in mongoose
+// req.body = {title,description}
+
+app.post('/api/notes', async (req,res) =>{
+    const {title,description} = req.body
+  
+   const note = await noteModel.create({
+        title,description
+    })
+  res.status(201).json({
+    message:"note created successfully",
+    note 
+  })
+})
+
+
+//GET /api/notes
+//Fetch all the data from mongodb and send them in the response
+
+app.get("/api/notes",async (req,res)=>{
+  const notes = await noteModel.find()
+
+  res.status(200).json({
+    message:"Notes fetched successfully",
+    notes
+  })
+})
+
+//DELETE /api/notes/:id
+//Delete note with the id from req.param
+
+app.delete('/api/notes/:id',async (req,res)=>{
+  const id = req.params.id
+  
+  await noteModel.findByIdAndDelete(id)
+
+  res.status(200).json({
+    message:"Note deleted successfully"
+  })
+})
+
+//PATCH /api/notes/:id
+//update the description of the note by id 
+// req.body = {description}
+
+app.patch('/api/notes/:id',async (req,res)=>{
+  const id = req.params.id
+  const {description} = req.body
+
+  await noteModel.findByIdAndUpdate(id,{description})
+
+  res.status(200).json({
+    messase:"Note updated successfully."
+  })
+})
+module.exports = app
