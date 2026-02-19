28171d1c6497adc986f7a5240fe31ca0b3e661cb
madhaviporte <madhaviporte2004@gmail.com>
2026-02-19 19:36:25 +0530

    update

--- Day-106/src/routes/post.routes.js
+++ Day-106/src/routes/post.routes.js
@@ -5,20 +5,28 @@ const multer = require("multer")
 const upload = multer({storage:multer.memoryStorage()})
 const identifyUser = require("../middlewares/auth.middlewear")
 
-//post/api/posts [protected]
+//@routes POST/api/posts/[protected]
 //req.body = {caption, image-file}
 
 postRouter.post("/",upload.single("image"), identifyUser , postController.createPostController)
 
 
-//GET /api/posts/[protected]
+//@routes GET /api/posts/[protected]
+//@description Get all the posts created by the user that the request come from. all
 
 postRouter.get("/",identifyUser, postController.getPostController)
 
 
-//GET/api/post/details/:postId
-// -return an detail about specific post with the id. also check wheather the post belongs to the user
+// @routes GET/api/post/details/:postId
+// @description return an detail about specific post with the id. also check wheather the post belongs to the user
 
 postRouter.get("/details/:postId",identifyUser, postController.getPostDetailsController)
+
+
+//@routes POST/api/posts/like/:postid
+//@description like a post with the id provided in the request params
+postRouter.post("/like/:postId",identifyUser,postController.likePostController)
+
+
 
 module.exports = postRouter 
