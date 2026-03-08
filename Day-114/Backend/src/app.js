97943c89c66a0d7139459162afebb7fe433056c0
madhaviporte <madhaviporte2004@gmail.com>
2026-03-09 02:46:27 +0530

    day-116

--- Day-114/Backend/src/app.js
+++ Day-114/Backend/src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cookiePasrser = require("cookie-parser");
+const cors = require("cors")
 
 const app = express()
 app.use(express.json());
 app.use(cookiePasrser());
+app.use(cors({
+    origin :"http://localhost:5173",
+    credentials: true
+}))
 
 // Routes
 
--- Day-114/Frontend/src/features/auth/pages/Login.jsx
+++ Day-114/Frontend/src/features/auth/pages/Login.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../style/login.scss"
 import FormGroup from '../components/FormGroup'
-import { Link } from 'react-router'
+import { Link, useNavigate } from 'react-router'
+import {useAuth} from '../hooks/useAuth'
 
 const Login = () => {
+
+    const {loading,handleLogin} = useAuth()
+
+    const navigate = useNavigate()
+
+    const [email,seteMail]=useState('')
+    const [password,setPassword] = useState("")
+
+    async function handleSubmit(e){
+        e.preventDefault()
+        await handleLogin({email,password})
+        navigate("/")
+    }
+
   return (
    <main className='login-page'>
     <div className="form-container">
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
 
-           <FormGroup label="Email" placeholder="Enter your email"/>
-           <FormGroup label="Password" placeholder="Enter your password"/>
+        <FormGroup
+           value={email}
+           onChange={(e)=>seteMail(e.target.value)}
+           label="Email" 
+           placeholder="Enter your email"/>
+
+        <FormGroup 
+           value={password}
+           onChange={(e)=>setPassword(e.target.value)}
+           label="Password" 
+           placeholder="Enter your password"/>
 
             <button className='button' type='submit'>Login</button>
             <p>Don't have an account?<Link to="/register">Register here</Link></p>
