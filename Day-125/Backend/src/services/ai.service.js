c069c4694ee92bca5a37b25b506e0a85046efb13
madhaviporte <madhaviporte2004@gmail.com>
2026-03-18 00:32:25 +0530

    Day-125

--- Day-125/Backend/src/services/ai.service.js
+++ Day-125/Backend/src/services/ai.service.js
@@ -1,12 +1,45 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import {ChatMistralAI} from "@langchain/mistralai"
+import {HumanMessage, SystemMessage, AIMessage} from "langchain";
 
-const model = new ChatGoogleGenerativeAI({
+const geminiModel = new ChatGoogleGenerativeAI({
     model: "gemini-2.5-flash-lite",
     apiKey: process.env.GEMINI_API_KEY
 });
 
-export async function testAi() {
-    const response = await model.invoke("Explain AI in simple words");
-    console.log(response.content);
-    return response.content;
+const mistralModel = new ChatMistralAI({
+    model: "mistral-small-latest",
+    apiKey:process.env.MISTRAL_API_KEY
+})
+
+
+export async function generateResponse(messages){
+    
+    const response = await geminiModel.invoke(messages.map(msg=>{
+        if(msg.role=="user")
+        {
+            return new HumanMessage(msg.content)
+        }else if(msg.role=="ai"){
+            return new AIMessage(msg.content)
+        }
+    }))
+    return response.text;
+}
+
+export async function generateChatTitle(message){
+    
+    const response = await mistralModel.invoke([
+        new SystemMessage(`
+        You are a helpful assistant that generates concise and descriptive titles for chat conversation.
+    
+        User will provide youu with the first message of chhat conversation, and you will generate a tittle that capture the essence of the conversation in 2-3 words. the title should be clear, relevant, and engaging, giving users a quick understanding of thhe chat's topic.
+        `),
+
+            new HumanMessage(`
+                Generate a title for a chat conversation based on the following first message:
+                "${message}"
+                `)
+    ])
+
+    return response.text;
 }
