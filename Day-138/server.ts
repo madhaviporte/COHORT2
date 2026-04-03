cd16e4c102276949c90ed2816e0a1a5c56988541
madhaviporte <madhaviporte2004@gmail.com>
2026-04-04 00:27:47 +0530

    Day-138

--- Day-138/server.ts
+++ Day-138/server.ts
@@ -1,5 +1,5 @@
+import "dotenv/config";
 import app from "./src/app.js";
-
 
 
 
--- Day-138/src/app.ts
+++ Day-138/src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import runGraph from './ai/graph.ai.js'
 
 const app = express()
 
 
-app.get('/', (req,res)=>{
-    res.send('Hello World')
+app.get('/', async (req,res)=>{
+ const result = await runGraph("write a code for Factorial funtion in js")
+
+ res.json(result)
 })
 
 export default app;
