3f23784f67fc049579bc7a606e72a320f3f6db3c
madhaviporte <madhaviporte2004@gmail.com>
2026-04-17 11:35:06 +0530

    day-143 bakend part

--- Day-143/Backend/src/controllers/auth.controller.js
+++ Day-143/Backend/src/controllers/auth.controller.js
@@ -7,13 +7,28 @@ async function sendTokenResponse(user, res) {
 
     const token = jwt.sign({
         id: user._id,
-    }, config.JWT_SECRET)
+    }, config.JWT_SECRET, {
+        expiresIn:'7d'
+    })
+res.cookie("token", token)
+
+    res.status(200).json({
+        message,
+        success: true,
+        user: {
+            id: user._id,
+            email: user.email,
+            contact: user.contact,
+            fullname: user.fullname,
+            role: user.role
+        }
+    })
 
 }
 
 
 export const register = async (req, res) => {
-    const { email, contact, password, fullname } = req.body;
+    const { email, contact, password, fullname,isSeller} = req.body;
 
     try {
         const existingUser = await userModel.findOne({
@@ -31,10 +46,11 @@ export const register = async (req, res) => {
             email,
             contact,
             password,
-            fullname
+            fullname,
+            role: isSeller ? "seller" : "buyer"
         })
 
-
+   await sendTokenResponse(user, res, "User registered successfully")
 
     } catch (error) {
         console.log(error)
