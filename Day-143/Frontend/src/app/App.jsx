c6da81df12c6f261a15417f77017ca44589466ed
madhaviporte <madhaviporte2004@gmail.com>
2026-04-17 19:57:28 +0530

    Day-143

--- Day-143/Frontend/src/app/App.jsx
+++ Day-143/Frontend/src/app/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import "./App.css"
+import { RouterProvider } from 'react-router'
+import { routes } from './app.routes'
 
 const App = () => {
-  return (
-    <>
-      <h1>hello world</h1>
-    </>
-  )
+  return <RouterProvider router={routes} />;
 }
 
 export default App
--- Day-143/Frontend/src/features/auth/hook/useAuth.js
+++ Day-143/Frontend/src/features/auth/hook/useAuth.js
@@ -1,5 +1,5 @@
 import { setError, setLoading, setUser } from "../state/auth.slice"
-import { register } from "../service/auth.api"
+import { register,login } from "../service/auth.api"
 import { useDispatch } from "react-redux"
 
 
@@ -16,6 +16,11 @@ export const useAuth = () => {
 
     }
 
-    return { handleRegister }
+    async function handleLogin({ email, password }) {
+        const data = await login({ email, password })
+        dispatch(setUser(data.user))
+    }
+
+    return { handleRegister, handleLogin }
 
 }
