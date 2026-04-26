215ca06d287af1194fe37a4905bf133c476f0ef2
madhaviporte <madhaviporte2004@gmail.com>
2026-04-26 19:03:42 +0530

    Day-151 Frontend

---
+++ Day-151/Frontend/src/app/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./App.css"
+import { RouterProvider } from 'react-router'
+import { routes } from './app.routes'
+import { useSelector } from 'react-redux'
+import { useAuth } from '../features/auth/hook/useAuth'
+import { useEffect } from 'react'
+
+const App = () => {
+
+   const { handleGetMe } = useAuth()
+
+  const user = useSelector(state => state.auth.user)
+
+  console.log(user)
+
+  useEffect(() => {
+    handleGetMe()
+  }, [])
+
+  return  (
+    <>
+      <RouterProvider router={routes} />
+    </>
+  )
+
+}
+
+export default App
