67453edfe8754ee8f304e0efbf3b7715bd722754
madhaviporte <madhaviporte2004@gmail.com>
2026-04-29 11:10:09 +0530

    .

--- Day-151/Frontend/src/main.jsx
+++ Day-151/Frontend/src/main.jsx
@@ -8,9 +8,7 @@ import { store } from './app/app.store.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-
     <App />
-    
     </Provider>
   </StrictMode>,
 )
