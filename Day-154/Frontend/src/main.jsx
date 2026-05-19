40535023e9a8b03b2abe153314b78af3495de1ef
madhaviporte <madhaviporte2004@gmail.com>
2026-05-19 17:32:17 +0530

    .

--- Day-154/Frontend/src/main.jsx
+++ Day-154/Frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from './app/App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './app/app.store.js'
 
+console.log("AUTH API BASE:", import.meta.env.VITE_API_URL)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- Day-155/Frontend/src/features/auth/service/auth.api.js
+++ Day-155/Frontend/src/features/auth/service/auth.api.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const authApiInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL + "/api/auth",
     withCredentials: true,
+
+
 })
 
 
