c59bf206f5b5d153034e7becdf70eba029d35b91
madhaviporte <madhaviporte2004@gmail.com>
2026-05-19 17:07:59 +0530

    .

--- Day-155/Frontend/src/features/products/service/product.api.js
+++ Day-155/Frontend/src/features/products/service/product.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const productApiInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL + "/api/products",
+    baseURL: import.meta.env.VITE_API_URL + "/api/auth",
     withCredentials: true,
 })
 
