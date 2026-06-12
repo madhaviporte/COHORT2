0e85b4747fc85b6c2f234488eb5f01be241972e8
madhaviporte <madhaviporte2004@gmail.com>
2026-06-12 22:01:30 +0530

    Docker

---
+++ Day-160/Backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import morgan from 'morgan'
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.get('/api/health', (req,res)=>{
+    res.send(200).json({status: "OK"})
+});
+
+app.get("/api/hello", (req,res) =>{
+    res.status(200).json({message: "Hello World!"})
+});
+
+app.get("/api/users",(req,res)=>{
+    const users = [
+        {id: 1, name: "Alice"},
+        {id: 2, name: "Bob"},
+        {id: 3, name: "Charlic"},
+    ]
+    res.status(200).json(users);
+})
+
+app.listen(3000, () => {
+    console.log("Server is runing on port 3000");
+})
